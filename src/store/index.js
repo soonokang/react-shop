8eb55b89ef48c99ad6f3ea90a8734ccea97a94e3
soonokang <ksokso1967@naver.com>
2023-10-16 17:26:43 +0900

    dist 생성

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userSlice } from "./user/user.slice";
-import { categoriesSlice } from "./categories/categories.slice";
-
+import userSlice from "./user/user.slice";
+import categoriesSlice from "./categories/categories.slice";
 export const store = configureStore({
   reducer: {
-    categoriesSlice,
     userSlice,
+    categoriesSlice,
   },
 });
